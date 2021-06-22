63f8f18ad89c52ec6ea259b108a46afdc9d31abf
Gent Gjylbegu <g3nt1706@gmail.com>
2021-06-22 09:58:04 +0000

    prog01.py

---
+++ prog01/prog01.py
@@ -0,0 +1,10 @@
+# https://www.programiz.com/python-programming/examples/add-number
+
+# this program adds two numbers
+
+num1 = input('Give first number: ')
+num2 = input('Give second number: ')
+
+sum = int(num1) + int(num2)
+
+print("The sum of {} and {} is: {}".format(num1, num2, sum))
