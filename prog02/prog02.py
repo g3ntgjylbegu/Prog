7ecb35d1b3cd137d4184fe65fc9f1eee4f81a50e
Gent Gjylbegu <g3nt1706@gmail.com>
2021-06-22 10:08:37 +0000

    prog02

---
+++ prog02/prog02.py
@@ -0,0 +1,9 @@
+# https://www.programiz.com/python-programming/examples/square-root
+
+# Finding the square root of numbers
+
+num = input("Given number: ")
+num = float(num)
+
+num_sqrt = num ** 0.5
+print("The square root of %0.3f is %0.3f"%(num, num_sqrt))
